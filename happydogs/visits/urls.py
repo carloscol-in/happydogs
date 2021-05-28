0f661a0685a392cd5256896593868fe051024345
Carlos Colin <contact@carloscolin.me>
2021-05-28 18:35:48 -0500

    added ListView to urls

--- happydogs/visits/urls.py
+++ happydogs/visits/urls.py
@@ -4,6 +4,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
+# Views
+from .views import (
+    ListVisitView,
+)
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'))
+    # path('', TemplateView.as_view(template_name='home.html')),
+    path('', ListVisitView.as_view(), name='visits'),
 ]
