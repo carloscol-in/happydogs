3ad70bbc5974ba0c327a92e1850e2c4de1706732
Carlos Colin <contact@carloscolin.me>
2021-05-28 12:03:49 -0500

    added constraints

--- happydogs/visits/models.py
+++ happydogs/visits/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+"""Visits models."""
 
-# Create your models here.
+# Django
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Utilities
+from utils.models import HappyDogsModel
+
+
+class Visit(HappyDogsModel):
+    """Visit model."""
+
+    start_date = models.DateField(
+        _('Start date'),
+    )
+    end_date = models.DateField(
+        _('End date'),
+        help_text='Date at which the boarding visit finishes'
+    )
+
+    dog = models.ForeignKey(
+        'dogs.Dog',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        """Meta option."""
+        constraints = [
+            models.UniqueConstraint(
+                fields=['start_date', 'end_date', 'dog'],
+                name='Start date and end date should be unique for each dog',
+            ),
+        ]
+
+    def clean(self):
+        """Don't allow overlapping start_date and end_date periods."""
