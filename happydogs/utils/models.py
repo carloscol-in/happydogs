71f51f90d2081f74b13b1b57a0b8fa020b1dffb5
Carlos Colin <contact@carloscolin.me>
2021-05-28 12:13:46 -0500

    modified model

--- happydogs/utils/models.py
+++ happydogs/utils/models.py
@@ -4,7 +4,7 @@
 from django.db import models
 
 # dependencies
-from django.utils.translation import getText_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class HappyDogsModel(models.Model):
