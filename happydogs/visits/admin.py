c8560ade171fdc17142441333a2cdcde9b347294
Carlos Colin <contact@carloscolin.me>
2021-05-28 12:11:37 -0500

    register Visit admin model

--- happydogs/visits/admin.py
+++ happydogs/visits/admin.py
@@ -1,3 +1,30 @@
+"""Visits admin."""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from .models import Visit
+
+
+class VisitAdmin(admin.ModelAdmin):
+    """Visit admin."""
+    list_display = ('dog', 'start_date', 'end_date',)
+    list_display_links = ('start_date', 'end_date',)
+    search_fields = ('dog',)
+
+    fieldsets = (
+        ('Dog', {
+            'fields': (
+                ('dog'),
+            )
+        }),
+        ('Boarding Visit', {
+            'fields': (
+                ('start_date', 'end_date'),
+            )
+        })
+    )
+
+
+admin.site.register(Visit, VisitAdmin)
