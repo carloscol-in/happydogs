93fe59f17a7e94c3de52225aac59714b2f7e7dc8
Carlos Colin <contact@carloscolin.me>
2021-05-28 12:00:08 -0500

    added Dog object str and get_full_name methods

--- happydogs/dogs/models.py
+++ happydogs/dogs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 # dependencies
-from happydogs.utils.models import HappyDogsModel
+from utils.models import HappyDogsModel
 
 
 class Dog(HappyDogsModel):
@@ -26,3 +26,11 @@ class Dog(HappyDogsModel):
                 name='Unique full name'
             )
         ]
+
+    def __str__(self):
+        """Get dog's full name."""
+        return self.get_full_name()
+
+    def get_full_name(self):
+        """Get dog's full name."""
+        return "{} {}".format(self.first_name, self.last_name)
