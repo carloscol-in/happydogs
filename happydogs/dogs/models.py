1b44107e2310771f5e656af1ef03ac505f0bb0c8
Carlos Colin <contact@carloscolin.me>
2021-05-28 11:04:29 -0500

    added dogs model and restriction

--- happydogs/dogs/models.py
+++ happydogs/dogs/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+"""Dogs models."""
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# dependencies
+from happydogs.utils.models import HappyDogsModel
+
+
+class Dogs(HappyDogsModel):
+    """Dogs model."""
+    first_name = models.CharField(
+        _('Dog first name'),
+        max_length=50,
+    )
+    last_name = models.CharField(
+        _('Dog last name'),
+        max_length=100,
+    )
+
+    class Meta:
+        """Meta option."""
+        constraints = [
+            models.UniqueConstraint(
+                fields=['first_name', 'last_name'],
+                name='Unique full name'
+            )
+        ]
