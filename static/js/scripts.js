7d06882b17da1fb262ef50637ab8a83b9fc56ce0
Carlos Colin <contact@carloscolin.me>
2021-05-28 23:55:05 -0500

    added functionality to consume get_dogs_on_day api method

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,6 +6,44 @@ $(function() {
         format:'yyyy-mm-dd',
     });
 
+    $('#calendar-dates').on('click', '.Calendar__col', function(e) {
+        e.preventDefault();
+        var long_date = $(this).next('input').val();
+
+        $.get(`http://localhost:8000/visits/${long_date}`, (data) => {
+            console.log('calling');
+            var offset_height = $('#calendar').outerHeight();
+            $('#dog-data').animate({
+                top: `${50 + offset_height}px`,
+                opacity: 1,
+            }, 1000, () => {})
+
+            var html = '';
+
+            for (const [_, dog_data] of Object.entries(data.dogs)){
+                console.log(dog_data);
+                html += `
+                    <div class="row d-flex flex-row">
+                        <div class="col-3 text-center">
+                            Dog ${dog_data.n + 1}
+                        </div>
+                        <div class="col-3 text-center">
+                            ${dog_data.fullname}
+                        </div>
+                        <div class="col-3 text-center">
+                            ${dog_data.startdate}
+                        </div>
+                        <div class="col-3 text-center">
+                            ${dog_data.enddate}
+                        </div>
+                    </div>
+                `;
+            }
+
+            $('#dog-data').html(html);
+        })
+    })
+
     $('#period-confirm').click((event) => {
         var period_start = $("#period-start").val();
         var period_end = $("#period-end").val();
@@ -22,9 +60,7 @@ $(function() {
             $('#calendar').animate({
                 top: '0px',
                 opacity: 1,
-            }, 1000, () => {
-                console.log('animated');
-            });
+            }, 1000, () => {});
 
             var html = '';
 
@@ -38,13 +74,12 @@ $(function() {
 
                 html += `
                     <div class="Calendar__col">
-                        <a class="Calendar__col--link" href="visits/${value.long_date}">
-                            <div class="Calendar__col--info d-flex flex-column justify-content-center">
-                                <small>${value.short_date}</small>
-                                <p>${value.dog_count} dogs</p>
-                            </div>
-                        </a>
+                        <div class="Calendar__col--info d-flex flex-column justify-content-center">
+                            <small>${value.short_date}</small>
+                            <p>${value.dog_count} dogs</p>
+                        </div>
                     </div>
+                    <input value="${value.long_date}" type="hidden"/>
                 `;
             }
             $('#calendar-dates').html(html);
