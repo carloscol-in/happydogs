8593b0754c209e025b78e6248b02544b3dcfddcf
Carlos Colin <contact@carloscolin.me>
2021-05-28 22:09:51 -0500

    cleaned views file

--- happydogs/visits/views.py
+++ happydogs/visits/views.py
@@ -13,51 +13,6 @@ from .models import Visit
 from datetime import datetime, timedelta
 
 
-class ListVisitView(ListView):
-    """List all visits day by day on given period."""
-
-    template_name = 'home.html'
-    model = Visit
-    context_object_name = 'visits'
-
-    def get_queryset(self):
-        """Customize queryset to return dict object where
-        keys are ordered days within the period, and values
-        the count of dogs."""
-        response = {"calendar": {}}
-
-        period_start_str = self.request.GET.get('period_start', None)
-        period_end_str = self.request.GET.get('period_end', None)
-
-        period_start = datetime.strptime(period_start_str, "%Y-%m-%d")
-        period_end = datetime.strptime(period_end_str, "%Y-%m-%d")
-
-        period_week_start = (period_start -
-                             timedelta(days=period_start.weekday()))
-        period_week_end = (period_end +
-                           timedelta(days=(7 - (period_end.weekday() + 1))))
-
-        for i in range(0, (period_week_end - period_week_start).days + 1):
-            curr_date = period_week_start + timedelta(i)
-
-            if(curr_date >= period_start and curr_date <= period_end):
-                response['calendar'].setdefault(
-                    curr_date.strftime("%Y-%m-%d"),
-                    {
-                        'short_date': curr_date.strftime("%b %d"),
-                        'dog_count': Visit.objects.filter(
-                            start_date__lte=curr_date,
-                            end_date__gte=curr_date)
-                        .count()
-                    }
-                )
-                continue
-
-            response['calendar'].setdefault(curr_date.strftime("%b %d"), '')
-
-        return response
-
-
 @require_http_methods(["GET"])
 def get_visits_on_period(request):
     """Json response for visits on given period."""
@@ -95,3 +50,32 @@ def get_visits_on_period(request):
 
     # return render(request, 'home.html', response, content_type='application/xhtml+xml')
     return JsonResponse(response, status=200, safe=False)
+
+
+@require_http_methods(['GET'])
+def get_dogs_on_day(request, date):
+    """Get the dogs full name on given day."""
+    response = {"dogs": []}
+    status_code = 200
+
+    # if date can be parsed to datetime, then continue
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except:
+        response = {
+            'error': 'You didn\'t wrote correctly the date.'
+        }
+        status_code = 404
+    else:
+        visits = Visit.objects.filter(start_date__lte=date, end_date__gte=date)
+
+        for n in range(0, len(visits)):
+            data = {
+                'n': n,
+                'fullname': visits[n].dog.get_full_name(),
+                'startdate': visits[n].start_date.strftime("%Y-%m-%d"),
+                'enddate': visits[n].end_date.strftime("%Y-%m-%d"),
+            }
+            response['dogs'].append(data)
+
+    return JsonResponse(response, status=status_code, safe=False)
