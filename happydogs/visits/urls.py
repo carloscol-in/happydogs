64e7ee7d04257264c642693eb9a7f4699bd09625
Carlos Colin <contact@carloscolin.me>
2021-05-28 22:08:08 -0500

    added view for get_dogs_on_day

--- happydogs/visits/urls.py
+++ happydogs/visits/urls.py
@@ -13,7 +13,7 @@ from .views import (
 
 urlpatterns = [
     # path('', TemplateView.as_view(template_name='home.html')),
-    path('', ListVisitView.as_view(), name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('visits/', get_visits_on_period, name='visits'),
     path('visits/<str:date>', get_dogs_on_day, name='detail'),
 ]
