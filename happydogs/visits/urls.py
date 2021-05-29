d700fa6b84d80552ba20827866ac2e8ca4df4987
Carlos Colin <contact@carloscolin.me>
2021-05-28 20:52:00 -0500

    added urls to function based views

--- happydogs/visits/urls.py
+++ happydogs/visits/urls.py
@@ -7,9 +7,13 @@ from django.views.generic import TemplateView
 # Views
 from .views import (
     ListVisitView,
+    get_visits_on_period,
+    get_dogs_on_day,
 )
 
 urlpatterns = [
     # path('', TemplateView.as_view(template_name='home.html')),
-    path('', ListVisitView.as_view(), name='visits'),
+    path('', ListVisitView.as_view(), name='home'),
+    path('visits/', get_visits_on_period, name='visits'),
+    path('visits/<str:date>', get_dogs_on_day, name='detail'),
 ]
